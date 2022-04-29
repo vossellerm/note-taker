6d00c36b580f3057a8d9472b9d4105711c3cbce8
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-29 01:14:29 -0700

    install uuid

--- Develop/server.js
+++ Develop/server.js
@@ -4,30 +4,30 @@ const { clog } = require("./middlewear/clog");
 console.log(typeof clog);
 const api = require("./routes/index.js");
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-// const app = express();
+const app = express();
 
 // middleware
-// app.use(clog);
+app.use(clog);
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api", api);
+app.use("/api", api);
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-// // GET Route for notes page
-// app.get("/", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/public/notes.html"))
-// );
+// GET Route for notes page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
-// // GET Route for homepage
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/public/index.html"))
-// );
+// GET Route for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-// app.listen(PORT, () =>
-//   console.log(`App listening at http://localhost:${PORT} 🚀`)
-// );
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
